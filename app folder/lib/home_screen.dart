ec89729847fb8a595a3245577f2d93ed4260136c
unknown <adkenny002@gmial.com>
2025-01-31 00:39:36 +0100

    home_screen file was added

---
+++ app folder/lib/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+            "hi there!, I'm APK \nand i'm working on a mobile app as my first task")
+      ],
+    );
+  }
+}
